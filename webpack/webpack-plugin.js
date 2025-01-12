4ff06b14049db847957f3f59154219ecd8d5d0cd
Jakub Eliáš <wurducius@gmail.com>
2025-01-12 02:21:29 +0100

    Add dependencies, base styles, minify scripts and such

--- webpack/webpack-plugin.js
+++ webpack/webpack-plugin.js
@@ -1,3 +1,5 @@
+const { join, read } = require("../src/util/fs")
+
 const pluginName = "Eofol5 webpack plugin"
 
 // eslint-disable-next-line no-unused-vars
@@ -51,6 +53,7 @@ const addAsset = (compilation) => (name, content, info) => {
 const optimizeAssets = (compiler, compilation) => {
   const addAssetImpl = addAsset(compilation)
   addAssetImpl("assets/js/eofol.js", "console.log('EOFOL RUNTIME CODE!!!')", {})
+  addAssetImpl("assets/css/base.css", read(join(process.cwd(), "src", "resources", "base.css")).toString(), {})
 }
 
 // eslint-disable-next-line no-unused-vars
