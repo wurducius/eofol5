3d11d81425c99be72aa6a38264e7f8ca9000c463
Jakub Eliáš <wurducius@gmail.com>
2025-01-14 23:40:37 +0100

    Add docker script

---
+++ docker/docker-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Eofol5 Docker test"
+echo "[1/3] Cloning Eofol5..."
+git clone https://github.com/wurducius/eofol5
+cd eofol5
+rm ./package-lock.json
+echo "[2/3] Installing Eofol5..."
+npm i
+echo "[3/3] Running Eofol5..."
+npm start
