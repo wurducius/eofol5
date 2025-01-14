aba1c39a3e50180c6969276505cbce828c12a043
Jakub Eliáš <wurducius@gmail.com>
2025-01-14 22:26:56 +0100

    Use eofol-head from npm, fill out head data

--- src/compile/template.js
+++ src/compile/template.js
@@ -2,7 +2,22 @@ const { join, parse, read, write } = require("../util/fs")
 const { head } = require("eofol-head")
 
 const precompileTemplate = (buildPath, projectPath) => async (viewName) => {
-  const headData = { title: "Eofol5 app" }
+  const headData = {
+    title: "Eofol5 app",
+    description: "All inclusive web framework with zero configuration, batteries included!",
+    keywords: "JS,Frontend framework",
+    author: "Jakub Eliáš",
+    favicon: "./assets/media/images/favicon.png",
+    appleTouchIcon: "./assets/media/images/logo-sm.png",
+    descriptionOg: "All inclusive web framework with zero configuration, batteries included!",
+    imageOg: "./assets/media/images/logo-lg.png",
+    imageTypeOg: "image/png",
+    imageHeightOg: "512",
+    imageWidthOg: "512",
+    language: "en",
+    manifest: "manifest.json",
+    themeColor: "#09090b",
+  }
   const htmlContent = read(join(projectPath, `${viewName}.html`)).toString()
   const stylesStatic = ""
   const htmlResult = await head(
