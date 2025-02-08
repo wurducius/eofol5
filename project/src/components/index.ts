7e4443fe033eb9b4e5fdf5371a7189fcfa7cc69a
Jakub Eliáš <wurducius@gmail.com>
2025-02-08 04:52:05 +0100

    Add lifecycle test

--- project/src/components/index.ts
+++ project/src/components/index.ts
@@ -3,9 +3,11 @@ import weatherx from "./weather"
 import forceUpdatex from "./force-update"
 import renderTestx from "./render-test"
 import storex from "./store"
+import lifecycleTestx from "./lifecycle-test"
 
 export const counter = counterx
 export const weather = weatherx
 export const forceUpdate = forceUpdatex
 export const renderTest = renderTestx
 export const store = storex
+export const lifecycleTest = lifecycleTestx
